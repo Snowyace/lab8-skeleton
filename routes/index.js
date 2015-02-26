bfbd2da3ea6ca4fbcf5cedbbbaa3a218c189f879
Allan Asis <aasis@ucsd.edu>
2015-02-26 15:33:14 -0800

    initial analytics lab

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var projects = require("../projects.json")
  */
 
 exports.view = function(req, res){
+  var randnum = Math.random();
+  if(randnum > 0.5){
+  	res.render("index",projects);
+  }else{
+  	res.render("index_alternate", projects);
+  }
   // Here you should implement your random landing page rendering functionality
-  res.render("index", projects);
 };
